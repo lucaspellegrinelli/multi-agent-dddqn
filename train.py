31864d5d499c3718b6ca38823e75f8669c129717
lucaspellegrinelli <lucas.pellegrinelli@hotmail.com>
2022-12-30 09:11:58 -0300

    Remove paralellism

--- train.py
+++ train.py
@@ -3,7 +3,6 @@ import torch
 import random
 import argparse
 import numpy as np
-import concurrent.futures
 from torch.utils.tensorboard import SummaryWriter
 
 from agents import SnakeRandomAgent, DDDQNAgent, Agent
@@ -34,7 +33,7 @@ def build_agent(cold_start: bool = True):
     )
 
 def play_game(agent_a: Agent, agent_b: Agent):
-    env = Snake(n_steps=250)
+    env = Snake(n_steps=100)
 
     info = { "a": [], "b": [] }
 
@@ -86,7 +85,6 @@ def evaluate(agent_a: Agent, agent_b: Agent, n_games: int, shuffle: bool = True)
     return { key: value / n_games for key, value in results.items() }
 
 if __name__ == "__main__":
-    N_PROCESSES = 5
     writer = SummaryWriter()
 
     agent_0 = build_agent(not args.load)
@@ -96,20 +94,18 @@ if __name__ == "__main__":
         agent_0.load("models/agent_a/qnet.pth", "models/agent_a/target_qnet.pth")
         agent_1.load("models/agent_b/qnet.pth", "models/agent_b/target_qnet.pth")
 
-    for game_i in range(0, args.num_games, N_PROCESSES):
-        with concurrent.futures.ProcessPoolExecutor(max_workers=N_PROCESSES) as executor:
-            # Randomly choose players for this game
-            player_a = random.choice([agent_0, agent_1, SnakeRandomAgent()])
-            player_b = random.choice([agent_0, agent_1, SnakeRandomAgent()])
+    for game_i in range(args.num_games):
+        # Randomly choose players for this game
+        player_a = random.choice([agent_0, agent_1, SnakeRandomAgent()])
+        player_b = random.choice([agent_0, agent_1, SnakeRandomAgent()])
 
-            # Play games
-            game_infos = executor.map(play_game, [player_a] * N_PROCESSES, [player_b] * N_PROCESSES)
+        # Play game
+        game_info = play_game(player_a, player_b)
 
-            # Update agents
-            for game_info in game_infos:
-                for info_a, info_b in zip(game_info["info"]["a"], game_info["info"]["b"]):
-                    player_a.learn(info_a["pre_obs"], info_a["action"], info_a["reward"], info_a["obs"], info_a["done"])
-                    player_b.learn(info_b["pre_obs"], info_b["action"], info_b["reward"], info_b["obs"], info_b["done"])
+        # Update agents
+        for info_a, info_b in zip(game_info["info"]["a"], game_info["info"]["b"]):
+            player_a.learn(info_a["pre_obs"], info_a["action"], info_a["reward"], info_a["obs"], info_a["done"])
+            player_b.learn(info_b["pre_obs"], info_b["action"], info_b["reward"], info_b["obs"], info_b["done"])
 
         if game_i % 1000 == 0:
             if game_i != 0 and args.save:
